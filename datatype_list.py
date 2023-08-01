e0e402166703fed5a76db685112932b31d45e54f
Your Name <you@example.com>
2023-08-01 18:22:33 +0900

    2023-08-01 스테이징

--- datatype_list.py
+++ datatype_list.py
@@ -1,3 +1,25 @@
-# slicing
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-pass
+# slicing (슬라이싱)
+thislist = ["apple", "watermelon", "cherry", "orange", "kiwi", "melon", "mango"]
+# len(thislist)
+# 7
+# type(thislist)
+# <class 'list'>
+# special variables
+# function variables
+# thislist[1:3]
+# ['banana', 'cherry']
+# thislist[:-1]
+# ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon']
+# thislist[1::2]
+# ['banana', 'orange', 'melon']
+# thislist[1:3]
+# ['banana', 'cherry']
+# thislist[1::3]
+
+thislist[1] = 'watermelon'
+thislist.sort()
+thislist.sort(reverse=True)
+pass  
+
+# change vlaue in list 
+
